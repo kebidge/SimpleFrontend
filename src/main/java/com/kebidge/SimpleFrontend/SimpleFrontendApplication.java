5bc3d5915371dfaa524ec860c0b649dad45ee755
Kevin Kutzner <kevin.kutzner@hft-stuttgart.de>
2022-06-03 18:51:06 +0200

    setup project

---
+++ src/main/java/com/kebidge/SimpleFrontend/SimpleFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.kebidge.SimpleFrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleFrontendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kebidge/SimpleFrontend/SimpleFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kebidge.SimpleFrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
